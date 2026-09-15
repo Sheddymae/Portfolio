16d25145d91ccc40cdbb418435061cc1775d8668
Sheddymae <125443020+Sheddymae@users.noreply.github.com>
2026-09-15 20:51:10 +0300

    Add responsive mobile hook

---
+++ src/hooks/use-mobile.jsx
@@ -0,0 +1,2 @@
+import { useEffect, useState } from 'react';
+export default function useMobile(breakpoint=760){const[mobile,setMobile]=useState(()=>typeof window!=='undefined'&&window.innerWidth<breakpoint);useEffect(()=>{const onResize=()=>setMobile(window.innerWidth<breakpoint);window.addEventListener('resize',onResize);return()=>window.removeEventListener('resize',onResize)},[breakpoint]);return mobile}
